f5279b6c28f9e9a0bbf1341163a1c8d24687842b
rashedInt32 <parvez08eee@gmail.com>
2019-11-17 01:45:34 +0600

    single page style done

--- src/pages/Single.js
+++ src/pages/Single.js
@@ -6,7 +6,7 @@ import Col from '../components/ui/Col';
 
 //Name, image, artists, Album and duration
 
-function Single({ location, match }) {
+function Single({ location, history }) {
   const [track] = useState(location.state.track);
 
   const { name, album, popularity, artists, duration_ms } = track;
@@ -16,28 +16,34 @@ function Single({ location, match }) {
     backgroundSize: "cover"
   };
 
+  const goBack = () => history.goBack();
+
   return (
     <>
       <div className="banner">
-        <div className="container">
-          <Row>
-            <Col takes={4}>
-              <div className="image" style={style}></div>
-            </Col>
-            <Col takes={8}>
-              <h2>{name}</h2>
-              <h5 className="pb-10 fc-secondary">
-                Artist:{" "}
-                {artists.map((artist, i) => (
-                  <span key={i}>{artist.name},</span>
-                ))}
-              </h5>
+        <p className="back" onClick={goBack}>
+          Back
+        </p>
+        <div className="container single">
+          <img src={album.images[0].url} alt="" />
+          <h2>{name}</h2>
+          <h5 className="pb-10 fc-secondary">
+            Artist:{" "}
+            {artists.map((artist, i) => (
+              <span className="artist" key={i}>
+                {artist.name}
+                <small>, </small>
+              </span>
+            ))}
+          </h5>
 
-              <p className="  d-block">Album: {album.name}</p>
-              <p>Duration: {moment(duration_ms).format("m:s")}m</p>
-              <p>Popularity: {popularity}%</p>
-            </Col>
-          </Row>
+          <p className="  d-block">Album: {album.name}</p>
+          <p>
+            Duration: <b>{moment(duration_ms).format("m:s")}m</b>
+          </p>
+          <p>
+            Popularity: <b>{popularity}%</b>
+          </p>
         </div>
       </div>
     </>
